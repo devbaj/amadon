a33a2dfb258e5fb9c46f5d7ab497dff35a9e3f4c
Brett Jory <bjory7@gmail.com>
2019-04-15 13:26:52 -0700

    alterations required by assignment Amadon completed

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -7,10 +7,28 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
+def process(request):
+    
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
+    item = Product.objects.get(id=request.POST["id"])
+    item_price = float(item.price)
+    total_charge = quantity_from_form * item_price
+    print(f"Charging credit card {total_charge}")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    request.session["charge"] = total_charge
+    if "manifest" in request.session:
+        request.session["manifest"] += quantity_from_form
+    else:
+        request.session["manifest"] = quantity_from_form
+        
+    if "tab" in request.session:
+        request.session["tab"] += total_charge
+    else:
+        request.session["tab"] = total_charge
+    
+    return redirect("/checkout")
+
+def checkout(request):
+    
+    
+    return render (request, "store/checkout.html")
